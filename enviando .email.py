4a5093ef4aa1d150f32c774ffaf9476290225ad5
Lucas Cadilhe <lucas_cadilhe@hotmail.com>
2023-05-16 11:02:12 -0300

    estudos profundos em python

---
+++ enviando .email.py
@@ -0,0 +1,31 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+#criação de um objeto de mensagem
+msg = MIMEMultipart()
+texto = "Estou enviando um email com Python"
+
+#Parâmetros
+senha = "M@ri@n@325190"
+msg['From'] = "lucas.cadilhe01@gmail.com"
+msg['To'] = "mamachagas12@gmail.com"
+msg['Subject'] = "Teste de email com Python"
+
+#criação do corpo da mensagem
+msg.attach(MIMEText(texto, 'plain'))
+
+#criação do servidor
+server = smtplib.SMTP('smtp.gmail.com: 587')
+server.starttls()
+
+#Login na conta para envio
+server.login(msg['From'], senha)
+
+#envio da mensagem
+server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+#encerramento do servidor
+server.quit()
+
+print('Mensagem enviada com sucesso')
