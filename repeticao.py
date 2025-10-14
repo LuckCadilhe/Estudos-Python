b0dae639575783b7ff628fc211e293d8914557fa
Lucas Cadilhe <lucas_cadilhe@hotmail.com>
2025-10-14 12:08:14 -0300

    estudo repetição

---
+++ repeticao.py
@@ -0,0 +1,22 @@
+##ESTUDO REPETIÇÃO FOR
+
+##for p in palavras:
+##    print (p, len(p))
+##-----------------------------------------------------
+##for i in range(0, 10, 2):
+##    print(i)
+##-----------------------------------------------------
+##for n in range(2, 10):
+##   for x in range(2, n):
+##        if n % x == 0:
+##            print(f'{n} igual a {x} * {n // x}')
+##            break
+##-----------------------------------------------------
+##for num in range(0,10):
+##    if num % 2 == 0:
+##        print(f'Encotrando o numero que é par {num}')
+##        continue
+##    print(f'Acabou encontrando um numero que é impar {num}')
+##-----------------------------------------------------
+
+
